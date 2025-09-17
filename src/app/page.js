ee7e7a2b4507f57d57ff7efb4890eb7906397032
faraz-github <farazahmad.email@gmail.com>
2025-09-17 13:02:58 +0530

    Add CTA

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import HomePageServices from "@/components/pages/homepage/HomePageServices";
 import HomePageAboutMe from "@/components/pages/homepage/HomePageAboutMe";
 import HomePageTestimonials from "@/components/pages/homepage/HomePageTestimonials";
 import HomePageBlog from "@/components/pages/homepage/HomePageBlog";
+import PageCTA from "@/components/layout/PageCTA";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <HomePageAboutMe sectionID={"about-me"} />
       <HomePageTestimonials sectionID="testimonials" />
       <HomePageBlog sectionID="blog" />
+      <PageCTA pageId={"page-cta"} />
     </>
   );
 }
---
+++ src/components/layout/PageCTA.jsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { useState } from "react";
+
+import { Grid, Stack } from "@mui/material";
+import ForwardToInboxSharpIcon from "@mui/icons-material/ForwardToInboxSharp";
+import CallSharpIcon from '@mui/icons-material/CallSharp';
+
+import Container from "@/components/layout/Container";
+import GetFreeConsultation from "@/components/popups/GetFreeConsultation";
+import SectionTitle from "@/components/layout/SectionTitle";
+
+import styles from "@/styles/components/PageCTA.module.css";
+
+const PageCTA = ({ pageId, textured = false }) => {
+  const [isGetFreeSessionOpen, setIsGetFreeSessionOpen] = useState(false);
+
+  return (
+    <section
+      id={pageId}
+      className={`${styles.pageCTA} ${
+        textured ? "secondaryNoiseSurfaceBackground" : ""
+      }`}
+    >
+      <Container className="container-y-padding">
+        <Grid container spacing={2}>
+          <Grid size={{ xs: 12, lg: 6 }}>
+            <Stack
+              spacing={4}
+              alignItems={{ xs: "center", lg: "flex-start" }}
+              textAlign={{ xs: "center", lg: "left" }}
+            >
+              <SectionTitle
+                subTitle={""}
+                subTitleColor={"var(--white)"}
+                title={"Get Help"}
+                titleColor={"var(--secondaryDark)"}
+              />
+              <h3 style={{ maxWidth: "450px" }}>
+                Facing Legal Issues? Get Expert Help Now!
+              </h3>
+            </Stack>
+          </Grid>
+          <Grid
+            size={{ xs: 12, lg: 6 }}
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
+          >
+            <Stack bgcolor={"var(--primaryDark)"} p={5} spacing={2} alignItems={"center"}>
+              <CallSharpIcon fontSize="large" sx={{color:"var(--white)"}}/>
+              <h3>+91-8956784556</h3>
+              <GetFreeConsultation
+                variant={"contained"}
+                color={"primary"}
+                title="Book Consultation"
+                btnEndIcon={<ForwardToInboxSharpIcon />}
+                size="large"
+                open={isGetFreeSessionOpen}
+                setOpen={setIsGetFreeSessionOpen}
+              />
+            </Stack>
+          </Grid>
+        </Grid>
+      </Container>
+    </section>
+  );
+};
+
+export default PageCTA;
