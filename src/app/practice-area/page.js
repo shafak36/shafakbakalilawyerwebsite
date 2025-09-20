00a085488a89e2321990447e0aecdb9647a87fee
faraz-github <farazahmad.email@gmail.com>
2025-09-20 12:49:15 +0530

    Add practice area page hero

---
+++ src/app/practice-area/page.js
@@ -0,0 +1,9 @@
+import PracticeAreaPageHero from "@/components/pages/practiceArea/PracticeAreaPageHero";
+
+export default function About() {
+  return (
+    <>
+      <PracticeAreaPageHero sectionID="hero" />
+    </>
+  );
+}
---
+++ src/components/pages/practiceArea/PracticeAreaPageHero.jsx
@@ -0,0 +1,82 @@
+"use client";
+
+import Image from "next/image";
+
+import { Box, Grid, Stack } from "@mui/material";
+
+import * as motion from "motion/react-client";
+
+import Container from "@/components/layout/Container";
+
+import practiceareaPagePicture from "../../../../public/practiceareapagepicture.webp";
+
+import styles from "@/styles/pages/PracticeAreapage.module.css";
+
+const PracticeAreaPageHero = ({ sectionID }) => {
+  return (
+    <section id={sectionID} className={styles.heroImageSection}>
+      {/* Video Overlay (optional - for better text contrast) */}
+      <div className={styles.heroOverlay}></div>
+
+      <Container>
+        <div className={styles.heroContent}>
+          <Grid
+            container
+            minHeight={"var(--fullViewportHeight)"}
+            marginTop={{
+              xs: "var(--header-offset-height)",
+              sm: "var(--header-offset-height)",
+              lg: 0,
+            }}
+            alignItems={"center"}
+            justifyContent={"center"}
+          >
+            <Grid size={12} maxWidth={1100}>
+              <Stack spacing={4}>
+                <motion.div
+                  initial={{ opacity: 0, y: 50 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.5 }}
+                >
+                  <Box position={"relative"} height={400}>
+                    <Image
+                      src={practiceareaPagePicture}
+                      alt={"Advocate Shafak Bakali"}
+                      fill
+                      sizes="(max-width: 600px) 100vw, (max-width: 1024px) 50vw, 33vw"
+                      style={{
+                        objectFit: "contain",
+                        transform: "scale(1)",
+                      }}
+                    />
+                  </Box>
+                </motion.div>
+                <h1
+                  style={{
+                    color: "var(--accent)",
+                    textAlign: "center",
+                  }}
+                >
+                  Practice Areas
+                </h1>
+                <p
+                  style={{
+                    color: "var(--white)",
+                    textAlign: "center",
+                  }}
+                >
+                  Explore our core legal services designed to protect your
+                  rights, resolve disputes, and provide clarity in complex legal
+                  matters. Each area is handled with precision, empathy, and
+                  strategic expertise.
+                </p>
+              </Stack>
+            </Grid>
+          </Grid>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default PracticeAreaPageHero;
