c280378a4daaf9477c5a99bb49d9e962d707323f
faraz-github <farazahmad.email@gmail.com>
2025-09-24 13:12:24 +0530

    Change CTA

--- src/components/layout/PageCTA.jsx
+++ src/components/layout/PageCTA.jsx
@@ -1,22 +1,18 @@
-"use client";
+import Image from "next/image";
 
-import { useState } from "react";
-
-import { Grid, Stack } from "@mui/material";
-import ForwardToInboxSharpIcon from "@mui/icons-material/ForwardToInboxSharp";
+import { Box, Button, Divider, Grid, Stack } from "@mui/material";
 import CallSharpIcon from "@mui/icons-material/CallSharp";
 
 import Container from "@/components/layout/Container";
-import GetFreeConsultation from "@/components/popups/GetFreeConsultation";
 import SectionTitle from "@/components/layout/SectionTitle";
 
 import { phoneNumber } from "@/constants/personalInformation";
 
+import advocateLogo from "../../../public/advocate-logo.webp";
+
 import styles from "@/styles/components/PageCTA.module.css";
 
 const PageCTA = ({ pageId, textured = false }) => {
-  const [isGetFreeSessionOpen, setIsGetFreeSessionOpen] = useState(false);
-
   return (
     <section
       id={pageId}
@@ -48,30 +44,63 @@ const PageCTA = ({ pageId, textured = false }) => {
               </h3>
             </Stack>
           </Grid>
-          <Grid
-            size={{ xs: 12, lg: 6 }}
-            display={"flex"}
-            justifyContent={"center"}
-            alignItems={"center"}
-          >
+          <Grid size={{ xs: 12, lg: 6 }}>
             <Stack
-              bgcolor={"var(--primaryDark)"}
-              p={5}
+              bgcolor={"var(--white)"}
+              p={2}
+              pb={4}
               spacing={2}
-              alignItems={"center"}
               sx={{ border: "4px double var(--white)" }}
+              height={"100%"}
+              maxWidth={{ xs: "100%", lg: "70%" }}
+              mx={"auto"}
+              alignItems={"center"}
             >
-              <CallSharpIcon fontSize="large" sx={{ color: "var(--white)" }} />
-              <h3>{phoneNumber}</h3>
-              <GetFreeConsultation
-                variant={"contained"}
-                color={"primary"}
-                title="Book Consultation"
-                btnEndIcon={<ForwardToInboxSharpIcon />}
-                size="large"
-                open={isGetFreeSessionOpen}
-                setOpen={setIsGetFreeSessionOpen}
+              <Stack
+                direction={"row"}
+                width={"100%"}
+                alignItems={"center"}
+                justifyContent={"space-between"}
+              >
+                <Box height={"100px"} width={"100px"} position={"relative"}>
+                  <Image
+                    src={advocateLogo}
+                    alt={"Advocate Shafak Bakali"}
+                    fill
+                    sizes="(max-width: 600px) 100vw, (max-width: 1024px) 50vw, 33vw"
+                    style={{
+                      objectFit: "contain",
+                      transform: "scale(1)",
+                    }}
+                  />
+                </Box>
+                <Stack alignItems={"flex-end"}>
+                  <h4 style={{ color: "var(--black)" }}>Adv. Shafak Bakali</h4>
+                  <p style={{ color: "var(--black)" }}>BCA, B.Com, LLB, LLM</p>
+                </Stack>
+              </Stack>
+              <Divider
+                sx={{ backgroundColor: "var(--black)", width: "100%" }}
               />
+              <p style={{ fontSize: "12px", color: "var(--black)" }}>
+                Criminal | Civil | Labour | Agreements | Registration | Notary |
+                Cyber Law
+              </p>
+              <Divider
+                sx={{ backgroundColor: "var(--black)", width: "100%" }}
+              />
+              <a href={`tel:${phoneNumber}`}>
+                <Button
+                  size="large"
+                  disableElevation
+                  variant="contained"
+                  color="primary"
+                  endIcon={<CallSharpIcon />}
+                  sx={{ borderRadius: "var(--high-rounded-mui-btn)" }}
+                >
+                  Call Now
+                </Button>
+              </a>
             </Stack>
           </Grid>
         </Grid>
